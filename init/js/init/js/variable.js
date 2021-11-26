f47010a10983e21622a1d1e81f154ac598fa8e96
Sudo-Rahman <keturdu71880@gmail.com>
2021-11-26 11:53:27 +0100

    Revert "Add files via upload"
    This reverts commit 05b86fddc5906da7dfdc03a9a46c53d8fa5f64a6.

--- init/js/init/js/variable.js
+++
@@ -1,5 +0,0 @@
-var etat_partie = false;// va permettre de savoir si la partie a commencé ou non
-var vectcentreMaison = new THREE.Vector2(0, 0); // var qui stock les coordonné du centre de la maison
-var paramPiste;
-var piste;
-var scene;
